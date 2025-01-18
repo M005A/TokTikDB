7905d72cb97dda785e264540dd18e4d71e2d79a8
M005A <machomusa1@gmail.com>
2025-01-18 14:51:29 -0800

    fixed uri issue

--- config/database.js
+++ config/database.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
+const uri = process.env.MONGO_URI;
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        console.log(process.env)
+        await mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
