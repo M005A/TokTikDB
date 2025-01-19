dc73a56662ffb91faaad5c3f00d84035614f0cd2
M005A <machomusa1@gmail.com>
2025-01-19 04:15:34 -0800

    gif getter

--- routes/gifRoutes.js
+++ routes/gifRoutes.js
@@ -1,30 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Gif = require('../models/Gifs.js');
+const Gif = require("../models/Gifs.js");
 
 // Get all users
-router.get('/', async (req, res) => {
-    try {
-        const gifs = await Gif.find();
-        res.json(gifs);
-    } catch (err) {
-        res.status(500).send('Server error');
-    }
+router.get("/", async (req, res) => {
+  const { name } = req.query;
+
+  try {
+    const data = await Gif.find({ name: name })
+    res.json(data);
+  } catch (err) {
+    res.status(500).send("Server error");
+  }
 });
 
 // Add a user
-router.post('/', async (req, res) => {
-    const { name, gifData } = req.body;
+router.post("/", async (req, res) => {
+  const { name, gifData } = req.body;
 
-    try {
-        const newGif = new Gif({ name, gifData });
-        await newGif.save();
-        res.status(201).json(newGif);
-    } catch (err) {
-        //res.status(500).send('Server error');
-    }
+  try {
+    const newGif = new Gif({ name, gifData });
+    await newGif.save();
+    res.status(201).json(newGif);
+  } catch (err) {
+    //res.status(500).send('Server error');
+  }
 });
-
-
 
 module.exports = router;
